2ff25c5c19cf5fa1e7db4e2f3bd8735c0666fc00
Tim Vinzing <tim.vinzing@web.de>
2019-04-27 16:45:51 +0200

    Controller für die datenangefangen

---
+++ src/main/java/de/hwr/fims_backend/controller/DataController.java
@@ -0,0 +1,16 @@
+package de.hwr.fims_backend.controller;
+
+import java.util.ArrayList;
+
+import de.hwr.fims_backend.data.customerdata.*;
+import de.hwr.fims_backend.data.advertisement.*;
+import de.hwr.fims_backend.data.services.*;
+
+public class DataController {
+	ArrayList<Verstorbener> Verstorbene = new ArrayList<Verstorbener>();
+	ArrayList<Auftraggeber> Auftraggeber = new ArrayList<Auftraggeber>();
+	ArrayList<Angehoeriger> Angehörige = new ArrayList<Angehoeriger>();
+	ArrayList<Abholung> Abholung = new ArrayList<Abholung>();
+	ArrayList<Trauerfeier> Trauerfeiern = new ArrayList<Trauerfeier>();
+	ArrayList<Zeitungsanzeige> Zeitungsanzeigen = new ArrayList<Zeitungsanzeige>();
+}
